613efdec975cf91fefee187a881ce053503b043e
Naveenknk444 <knaveen@live.com>
2024-12-18 16:43:56 -0500

    Update Class1.cs

--- BlackJackGameLogic/Class1.cs
+++ BlackJackGameLogic/Class1.cs
@@ -2,6 +2,14 @@
 {
     public class Class1
     {
+        <xsl:template match="Citation">
+    <p class="pn-poms-citation">
+        <b>CITATIONS:</b>
+        <xsl:text> </xsl:text>
+        <xsl:apply-templates/>
+    </p>
+</xsl:template>
+
 
     }
 }
