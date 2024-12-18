997296ebf1ff4801731e053cc369e7f7e2f1005b
Naveenknk444 <knaveen@live.com>
2024-12-18 17:00:37 -0500

    Update Class1.cs

--- BlackJackGameLogic/Class1.cs
+++ BlackJackGameLogic/Class1.cs
@@ -4,15 +4,14 @@
     {
 
 
-        <xsl:template match="Citation" mode="#all">
-    <div class="pn-poms-citation">
+<xsl:template match="Citation" mode="#all">
+    <p class="pn-poms-citation">
         <span class="pn-poms-citation-title" style="font-weight: bold;">CITATIONS:</span>
         <xsl:text> </xsl:text>
-        <span class="pn-poms-citation-text">
-            <xsl:apply-templates/>
-        </span>
-    </div>
+        <xsl:apply-templates/>
+    </p>
 </xsl:template>
+
 
 
 
