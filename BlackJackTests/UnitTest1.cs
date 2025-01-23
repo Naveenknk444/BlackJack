a124a2be573c74521cf8de22299b4faa1ceb2b1f
Naveenknk444 <knaveen@live.com>
2025-01-22 23:40:02 -0500

    Update UnitTest1.cs

--- BlackJackTests/UnitTest1.cs
+++ BlackJackTests/UnitTest1.cs
@@ -1,8 +1,8 @@
 [Test]
-public void TestSaxonTransform_WithValidXmlAndXslt()
+public void TestSaxonTransform_WithMalformedXml()
 {
-    // Arrange: Valid XML and XSLT
-    string xmlInput = "<?xml version=\"1.0\"?><root><node>Test Content</node></root>";
+    // Arrange: Malformed XML
+    string xmlInput = "<?xml version=\"1.0\"?><root><node>Missing Closing Tag";
     string xsltInput = @"<xsl:stylesheet version=""1.0"" xmlns:xsl=""http://www.w3.org/1999/XSL/Transform"">
                             <xsl:template match=""/"">
                                 <html><body><xsl:value-of select=""//node"" /></body></html>
@@ -12,10 +12,7 @@ public void TestSaxonTransform_WithValidXmlAndXslt()
     var xsltExecutable = processor.NewXsltCompiler().Compile(new StringReader(xsltInput));
     bool saveLocal = false;
 
-    // Act
+    // Act & Assert: Expect the method to return null due to malformed XML
     var result = SaxonTransform(xmlInput, xsltExecutable, processor, saveLocal);
-
-    // Assert
-    Assert.That(result, Is.EqualTo("<html><body>Test Content</body></html>"), 
-        "The transformed output does not match the expected HTML.");
+    Assert.That(result, Is.Null, "The method did not handle malformed XML correctly.");
 }
