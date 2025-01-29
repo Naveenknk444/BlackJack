46be2acfe3f2c48f0c10f1ea13de893a82d60601
Naveenknk444 <knaveen@live.com>
2025-01-29 17:10:37 -0500

    Update UnitTest1.cs

--- BlackJackTests/UnitTest1.cs
+++ BlackJackTests/UnitTest1.cs
@@ -1,18 +1,2 @@
-[Test]
-public async Task ExtractAll_ShouldReturnValidFilenames()
-{
-    // Arrange
-    var source = new HallexDataMigratorSource(PolicyNetHtmlOrigin.LegacyWebsiteWithLocalAutosave, null);
 
-    // Act
-    var extractedRecords = await source.ExtractAll(CancellationToken.None);
 
-    // Assert
-    Assert.Multiple(() =>
-    {
-        foreach (var record in extractedRecords.Take(10)) // Check only first 10 records to optimize performance
-        {
-            Assert.That(record.filename, Does.EndWith(".json"), $"Filename {record.filename} does not have .json extension.");
-        }
-    });
-}
