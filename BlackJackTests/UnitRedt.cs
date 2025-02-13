dca1017e12ebbb1d460a42b5e983771163e7321c
Naveenknk444 <knaveen@live.com>
2025-02-13 13:29:02 -0500

    Update UnitRedt.cs

--- BlackJackTests/UnitRedt.cs
+++ BlackJackTests/UnitRedt.cs
@@ -1,3 +1,21 @@
-dotnet add package Microsoft.Extensions.Configuration
-dotnet add package Microsoft.Extensions.Configuration.Json
-dotnet add package Microsoft.Extensions.Configuration.FileExtensions
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+private Dictionary<string, string> GetLinksFromXML(string xmlContent)
+{
+    var doc = XDocument.Parse(xmlContent);
+    var links = new Dictionary<string, string>();
+
+    foreach (var element in doc.Descendants().Where(e => e.Attribute("link") != null))
+    {
+        var targetDoc = element.Attribute("targetDoc")?.Value;
+        var targetRef = element.Attribute("targetRef")?.Value;
+
+        if (!string.IsNullOrEmpty(targetDoc) && !string.IsNullOrEmpty(targetRef))
+        {
+            links[targetDoc] = targetRef;
+        }
+    }
+
+    return links;
+}
