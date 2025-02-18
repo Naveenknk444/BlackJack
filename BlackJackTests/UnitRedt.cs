0064a4eebed4de0ce319d88b24a3b7c10fc9371e
Naveenknk444 <knaveen@live.com>
2025-02-17 23:07:51 -0500

    Update UnitRedt.cs

--- BlackJackTests/UnitRedt.cs
+++ BlackJackTests/UnitRedt.cs
@@ -1,6 +1,4 @@
-private string CreateQuery(List<Link> links)
-{
-    var conditions = links.Select(link => $"(targetDoc = '{link.TargetDoc}' AND targetRef = '{link.TargetRef}')");
-    var query = $"SELECT 1 FROM YourTable WHERE " + string.Join(" OR ", conditions) + " LIMIT 1";
-    return query;
-}
+var conditions = linkRefs.SelectMany(link => 
+    link.targetDocs.Select(targetDoc => $"(targetDoc = '{targetDoc}' AND targetRef = '{link.targetRef}')")
+).ToList();
+var query = string.Join(" OR ", conditions);
