7f6d7f66d8e02ce4bb47d1cf0a3878376e51cc03
Naveenknk444 <knaveen@live.com>
2025-02-18 06:17:28 -0500

    Update UnitRedt.cs

--- BlackJackTests/UnitRedt.cs
+++ BlackJackTests/UnitRedt.cs
@@ -1,4 +1,26 @@
-     // Extract all Link elements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string xml = @"<RegReferences>
+                        <Link TargetDoc='CFR' TargetRef='CFR-20-404-316-C'>404.316(c)</Link>
+                        <Link TargetDoc='CFR' TargetRef='CFR-20-404-328'>404.328</Link>
+                        <Link TargetDoc='CFR' TargetRef='CFR-20-404-337-C'>404.337(c)</Link>
+                        <Link TargetDoc='CFR' TargetRef='CFR-20-404-352-D'>404.352(d)</Link>
+                        <Link TargetDoc='CFR' TargetRef='CFR-20-404-902-S'>404.902(s)</Link>
+                        <Link TargetDoc='CFR' TargetRef='CFR-20-404-1586-B'>404.1586(b)</Link>
+                        <Link TargetDoc='CFR' TargetRef='CFR-20-416-1338'>416.1338</Link>
+                        <Link TargetDoc='CFR' TargetRef='CFR-20-416-1402-J'>416.1402(j)</Link>
+                      </RegReferences>";
+
+        XDocument doc = XDocument.Parse(xml);
+        
+        // Extract all Link elements
         var links = doc.Descendants("Link")
                        .Select(link => new
                        {
@@ -13,3 +35,5 @@
         {
             Console.WriteLine($"TargetDoc: {link.TargetDoc}, TargetRef: {link.TargetRef}, Text: {link.Text}");
         }
+    }
+}
