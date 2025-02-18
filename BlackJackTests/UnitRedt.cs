bea53f2d170f79ff75fe7c3773d9f362033132ff
Naveenknk444 <knaveen@live.com>
2025-02-17 22:23:16 -0500

    Update UnitRedt.cs

--- BlackJackTests/UnitRedt.cs
+++ BlackJackTests/UnitRedt.cs
@@ -1,18 +1,1 @@
-    // Moved method: Fetch existing records from the Anchor table
-    public async Task<HashSet<(string TargetDoc, string TargetRef)>> FetchExistingAnchorsAsync(
-        List<(string TargetDoc, string TargetRef)> linkPairs)
-    {
-        if (!linkPairs.Any())
-            return new HashSet<(string, string)>();
-
-        var targetDocs = linkPairs.Select(e => e.TargetDoc).ToHashSet();
-        var targetRefs = linkPairs.Select(e => e.TargetRef).ToHashSet();
-
-        using var db = _dbContextFactory.CreateDbContext();
-        var existingAnchors = await db.Anchor
-            .Where(a => targetDocs.Contains(a.ClcthNm) && targetRefs.Contains(a.AnkrNum))
-            .Select(a => new { a.ClcthNm, a.AnkrNum })
-            .ToListAsync();
-
-        return new HashSet<(string, string)>(existingAnchors.Select(a => (a.ClcthNm, a.AnkrNum)));
-    }
+SELECT 1 FROM YourTable WHERE (targetDoc = 'CFR' AND targetRef = 'CFR-20-404-315(a)(1)(i)') OR (targetDoc = 'CFR' AND targetRef = 'CFR-20-404-327') OR (targetDoc = 'CFR' AND targetRef = 'CFR-20-404-352') OR (targetDoc =
