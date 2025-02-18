6d2f8a79a677e6962bb73316ed1e33424ffe5f5c
Naveenknk444 <knaveen@live.com>
2025-02-17 23:18:12 -0500

    Update UnitRedt.cs

--- BlackJackTests/UnitRedt.cs
+++ BlackJackTests/UnitRedt.cs
@@ -1,4 +1,9 @@
-var conditions = linkRefs.SelectMany(link => 
-    link.targetDocs.Select(targetDoc => $"(targetDoc = '{targetDoc}' AND targetRef = '{link.targetRef}')")
-).ToList();
+var conditions = linkRefs.SelectMany(link =>
+{
+    var targetDocsField = typeof(LinkRef).GetField("targetDoc", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+    var targetDocs = targetDocsField?.GetValue(link) as Collection<string>;
+
+    return targetDocs?.Select(targetDoc => $"(targetDoc = '{targetDoc}' AND targetRef = '{link.targetRef}')") ?? Enumerable.Empty<string>();
+}).ToList();
+
 var query = string.Join(" OR ", conditions);
